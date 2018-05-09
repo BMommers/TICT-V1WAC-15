ac77e2e573b20eb441287a383f969b87158e038a
Bart <bart.mommers@student.hu.nl>
2018-05-09 17:07:17 +0200

    Fixed main page. Overview is updated automatically

--- src/main/java/nl/hu/v1wac/firstapp/servlets/LandingPage.java
+++ src/main/java/nl/hu/v1wac/firstapp/servlets/LandingPage.java
@@ -1,26 +1,18 @@
 package nl.hu.v1wac.firstapp.servlets;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.*; import javax.servlet.http.*; import javax.servlet.ServletException;
+import java.net.*; import java.nio.file.*; import javax.servlet.annotation.WebServlet;
 
 
-@WebServlet(urlPatterns = "/landingpage")
+@WebServlet(urlPatterns = "/main")
 public class LandingPage extends HttpServlet {
 
-    private static final String htmlTemplate = "<!DOCTYPE html><html><title>LandingPage 1.0</title><body>%s</body></html>";
+    private static final String htmlTemplate = "<!DOCTYPE html><html><title>FileBrowser 1.0</title><body>%s</body></html>";
     private static final String dirTemplate = "<label><a href=\"%s\">%s</a></label><br/>";
-    private static final Path resourceBase = Paths.get("D:\\Librarys\\intelliJ-workspace\\TICT-V1WAC-15\\src\\main\\webapp");
+//    private static final String dirTemplate = "<label><a href=\"?path=%s\">%s</a></label><br/>";
+    //    private static final String fileTemplate = "<label>%s</label><br/>";
+    private static final String fileTemplate = "<label><a href=\"?path=%s\">%s</a></label><br/>";
+    private static final Path resourceBase = Paths.get("d:/librarys/intelliJ-workspace/TICT-V1WAC-15/src/main/webapp");
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String pathParam = request.getParameter("path");
@@ -30,10 +22,17 @@ public class LandingPage extends HttpServlet {
             requestedPath = requestedPath.toRealPath();
             StringBuilder responseBuilder = new StringBuilder();
             for (File dir : requestedPath.toFile().listFiles(file -> file.isDirectory())) {
-                if (dir.getName() != "WEB-INF") {
-                    responseBuilder.append(String.format(dirTemplate, dir.getName(), dir.getName()));
+                if (!dir.getName().contains("images") && !dir.getName().contains("WEB-INF")) {
+                    String encodedPath = URLEncoder.encode("" + dir.getName(), "UTF-8");
+                    responseBuilder.append(String.format(dirTemplate, encodedPath, dir.getName()));
                 }
+//                String encodedPath = URLEncoder.encode(decodedParam + "/" + dir.getName(), "UTF-8");
+//                responseBuilder.append(String.format(dirTemplate, encodedPath, dir.getName()));
             }
+//            for (File file : requestedPath.toFile().listFiles(file -> file.isFile())) {
+//                String encodedPath = URLEncoder.encode(decodedParam + "/" + file.getName(), "UTF-8");
+//                responseBuilder.append(String.format(fileTemplate, encodedPath, file.getName()));
+//            }
             response.setContentType("text/html");
             PrintWriter pw = response.getWriter();
             pw.write(String.format(htmlTemplate, responseBuilder.toString()));
