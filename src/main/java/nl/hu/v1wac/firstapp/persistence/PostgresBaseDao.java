cb8241c5d253c5d35b4e54a23ab20dcb2955adb5
Bart <bart.mommers@student.hu.nl>
2018-06-03 20:39:57 +0200

    Possible fix for Heroku

--- src/main/java/nl/hu/v1wac/firstapp/persistence/PostgresBaseDao.java
+++ src/main/java/nl/hu/v1wac/firstapp/persistence/PostgresBaseDao.java
@@ -8,11 +8,13 @@ public class PostgresBaseDao {
 
     protected final Connection getConnection() {
         Connection result = null;
-
+        System.out.println("Start Try");
         try {
+            System.out.println("Initiate ic");
             InitialContext ic = new InitialContext();
+            System.out.println("inititialize ds");
             DataSource ds = (DataSource) ic.lookup("java:comp/env/jdbc/PostgresDS");
-
+            System.out.println("save results");
             result = ds.getConnection();
         } catch (Exception ex) {
             throw new RuntimeException(ex);
